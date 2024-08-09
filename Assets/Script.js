e5f6521aea46a3756c270adf902eba789dd748bc
Priscilla Akaniro <akaniro.priscillia@gmail.com>
2024-08-09 14:11:33 +0100

    new note feature

--- Assets/Script.js
+++ Assets/Script.js
@@ -1,3 +1,4 @@
+//to move the side bar 
 const mainHead = document.querySelector( '.main-head' );
 const showcase = document.querySelector( '.showcase' );
 const toggler = document.querySelector( '.toggler' );
@@ -5,3 +6,52 @@ toggler.addEventListener( 'click', function(){
     mainHead.classList.toggle( 'active' );
     showcase.classList.toggle( 'width' );
 } )
+
+//to create new notes
+const notescontainer = document.querySelector('.notes-container')
+const createbtn = document.querySelector('#btn')
+let notes=document.querySelectorAll('.inputbox')
+
+createbtn.addEventListener("click", ()=>{
+    let inputbox = document.createElement("p")
+    let img=document.createElement("img")
+    // creates a p tag and sets the attibutes and class name 
+    inputbox.className="input-box"
+    inputbox.setAttribute("contenteditable","true")
+    // appens the delete icon into the input-box
+    img.src ="Assets/delete.png"
+    notescontainer.appendChild(inputbox).appendChild(img)
+    let note1=true
+})
+    function note(){
+        if(note1===true)
+        {
+            notescontainer.insertAdjacentElement("afterend",inputbox,img)
+        }
+
+    }
+
+//to delete note 
+notescontainer.addEventListener("click", function(e){
+    //if the target you click on has a tag name img then remove the parent element ie the whole p tag
+    if (e.target.tagName ==="IMG")
+{
+    e.target.parentElement.remove();
+}
+})
+
+//to-do list
+const inputlist= document.getElementById("input-list")
+const listconatiner= document.getElementById("list-conatiner")
+
+function addtask(){
+    if(inputlist.value === "")
+    {
+        alert("You havent added a task")
+    }else{
+        let li = document.createElement("li")
+        li.innerHTML = inputlist.value
+        listconatiner.appendChild(li)
+    }
+}
+
